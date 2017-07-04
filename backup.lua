3df78a31efbb49bd0cdd8f2470de52f4c75ef163
miladsadeghii <milad.n161bot@gmail.com>
2017-07-04 17:55:34 +0430

    Add files via upload

---
+++ backup.lua
@@ -0,0 +1,15 @@
+﻿
+local function run(msg, matches)
+   if matches[1]:lower() == 'backup' and is_sudo(msg) then
+ send_document(get_receiver(msg), "/root/Tabchi-plus/bot/tabchi.json", ok_cb, false)
+return '<b>Your Robot backup groups...</b>'
+end
+end
+return {
+  patterns = {
+    "^([Bb][Aa][Cc][Kk][Uu][Pp])",
+  },
+  run = run
+}
+
+-- by :@Bz _ 2Nafare _ info
